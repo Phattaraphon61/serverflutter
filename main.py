019e955f8b6f39f12e02836673b33aab4a777073
Phattaraphon Pewruang <phattaraphon.pe.61@ubu.ac.th>
2020-12-29 19:02:40 +0700

    test09

--- main.py
+++ main.py
@@ -1,18 +1,9 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
-from numpy import sign
 from starlette.middleware.cors import CORSMiddleware
 app = FastAPI()
 origins = [
     "*",
-    "https://commath-phattaraphon.herokuapp.com",
-    "https://commath-phattaraphon.herokuapp.com/b2s",
-    "https://commath-phattaraphon.herokuapp.com/elimination",
-    "https://commath-phattaraphon.herokuapp.com/interpolation",
-    "https://commath-phattaraphon.herokuapp.com/differentiation",
-    "https://commath-phattaraphon.herokuapp.com/integration",
-    "https://commath-phattaraphon.herokuapp.com/root-finding"
-
 ]
 
 
@@ -25,4 +16,4 @@ app.add_middleware(
 )
 @app.get("/")
 def home():
-    return {"22222"}
+    return {"333"}

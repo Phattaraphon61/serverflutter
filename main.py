7022cfc8e0b76df86db3ae43bf591e74ff8b1efa
Phattaraphon Pewruang <phattaraphon.pe.61@ubu.ac.th>
2021-01-01 01:30:07 +0700

    test18

--- main.py
+++ main.py
@@ -32,21 +32,21 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-# class PyObjectId(ObjectId):
+class PyObjectId(ObjectId):
 
-#     @classmethod
-#     def __get_validators__(cls):
-#         yield cls.validate
+    @classmethod
+    def __get_validators__(cls):
+        yield cls.validate
 
-#     @classmethod
-#     def validate(cls, v):
-#         if not ObjectId.is_valid(v):
-#             raise ValueError('Invalid objectid')
-#         return ObjectId(v)
+    @classmethod
+    def validate(cls, v):
+        if not ObjectId.is_valid(v):
+            raise ValueError('Invalid objectid')
+        return ObjectId(v)
 
-#     @classmethod
-#     def __modify_schema__(cls, field_schema):
-#         field_schema.update(type='string')
+    @classmethod
+    def __modify_schema__(cls, field_schema):
+        field_schema.update(type='string')
 # class Users(BaseModel):
 #     id: Optional[PyObjectId] = Field(alias='_id')
 #     tt: Optional[PyObjectId] = Field(alias='id')
@@ -69,7 +69,7 @@ app.add_middleware(
 
 @app.get("/")
 def home():
-    return {"666"}
+    return {"777"}
 # @app.get("/{id}")
 # async def read_root(id:str):
 #     tt = []

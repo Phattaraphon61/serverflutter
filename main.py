0c2de1c252025a4c1e13a71c8347ed4b16554c4d
Phattaraphon Pewruang <phattaraphon.pe.61@ubu.ac.th>
2021-01-01 01:32:54 +0700

    test19

--- main.py
+++ main.py
@@ -47,25 +47,25 @@ class PyObjectId(ObjectId):
     @classmethod
     def __modify_schema__(cls, field_schema):
         field_schema.update(type='string')
-# class Users(BaseModel):
-#     id: Optional[PyObjectId] = Field(alias='_id')
-#     tt: Optional[PyObjectId] = Field(alias='id')
-#     image: str
-#     # email:str
-#     class Config:
-#         arbitrary_types_allowed = True
-#         json_encoders = {
-#             ObjectId: str
-#         }
-# class Singup(BaseModel):
-#     name: str
-#     email:str
-#     password: str
-# class Singin(BaseModel):
-#     email:str
-#     password: str    
-# class CheckEmail(BaseModel):
-#     email:str 
+class Users(BaseModel):
+    id: Optional[PyObjectId] = Field(alias='_id')
+    tt: Optional[PyObjectId] = Field(alias='id')
+    image: str
+    # email:str
+    class Config:
+        arbitrary_types_allowed = True
+        json_encoders = {
+            ObjectId: str
+        }
+class Singup(BaseModel):
+    name: str
+    email:str
+    password: str
+class Singin(BaseModel):
+    email:str
+    password: str    
+class CheckEmail(BaseModel):
+    email:str 
 
 @app.get("/")
 def home():
